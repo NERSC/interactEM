8910babdc593df8d7a10795d7b1a719ff781b934
Sam Welborn <swelborn@lbl.gov>
2024-06-06 13:26:28 -0400

    fix fixtures

--- zmglue/fixtures.py
+++ zmglue/fixtures.py
@@ -1,34 +1,92 @@
 from uuid import uuid4
 
-from zmglue.types import EdgeJSON, PipelineJSON, Protocol, URIBase, URILocation
+from zmglue.pipeline import Pipeline
+from zmglue.types import EdgeJSON, OperatorJSON, PipelineJSON, PortJSON, PortType
 
-NODE_0_ID = uuid4()
-NODE_0 = NodeJSON(
-    id=NODE_0_ID,
+OPERATOR_0_ID = uuid4()
+OPERATOR_0_OUTPUT_0_ID = uuid4()
+OPERATOR_0_OUTPUT_1_ID = uuid4()
+
+OPERATOR_1_ID = uuid4()
+OPERATOR_1_INPUT_0_ID = uuid4()
+
+OPERATOR_2_ID = uuid4()
+OPERATOR_2_INPUT_0_ID = uuid4()
+
+OPERATOR_0 = OperatorJSON(
+    id=OPERATOR_0_ID,
     params={"hello": "world"},
-    outputs={
-        "out1": URIBase(
-            id=NODE_0_ID,
-            location=URILocation.node,
-            hostname="localhost",
-            interface="lo0",
-            protocol=Protocol.tcp,
-            portkey="out1",
-        )
-    },
+    outputs=[OPERATOR_0_OUTPUT_0_ID, OPERATOR_0_OUTPUT_1_ID],
 )
 
-NODE_1_ID = uuid4()
-NODE_1 = NodeJSON(id=NODE_1_ID, params={"hello": "world"}, inputs=["in1"])
+OPERATOR_0_PORT_0 = PortJSON(
+    id=OPERATOR_0_OUTPUT_0_ID,
+    operator_id=OPERATOR_0_ID,
+    port_type=PortType.output,
+    portkey="out1",
+)
+
+OPERATOR_0_PORT_1 = PortJSON(
+    id=OPERATOR_0_OUTPUT_1_ID,
+    operator_id=OPERATOR_0_ID,
+    port_type=PortType.output,
+    portkey="out1",
+)
+
+OPERATOR_1 = OperatorJSON(
+    id=OPERATOR_1_ID,
+    params={"hello": "world"},
+    inputs=[OPERATOR_1_INPUT_0_ID],
+)
+
+OPERATOR_1_PORT_0 = PortJSON(
+    id=OPERATOR_1_INPUT_0_ID,
+    operator_id=OPERATOR_1_ID,
+    port_type=PortType.input,
+    portkey="in1",
+)
+
+OPERATOR_2 = OperatorJSON(
+    id=OPERATOR_2_ID,
+    params={"hello": "world"},
+    inputs=[OPERATOR_2_INPUT_0_ID],
+)
+
+OPERATOR_2_PORT_0 = PortJSON(
+    id=OPERATOR_2_INPUT_0_ID,
+    operator_id=OPERATOR_2_ID,
+    port_type=PortType.input,
+    portkey="in1",
+)
 
 EDGE_0 = EdgeJSON(
-    nodes=NodePair(input_id=NODE_0.id, output_id=NODE_1.id),
-    ports=PortPair(input_portkey="out1", output_portkey="in1"),
+    input_id=OPERATOR_0_ID,
+    output_id=OPERATOR_0_OUTPUT_0_ID,
 )
+
+EDGE_1 = EdgeJSON(
+    input_id=OPERATOR_0_ID,
+    output_id=OPERATOR_0_OUTPUT_1_ID,
+)
+
+EDGE_2 = EdgeJSON(
+    input_id=OPERATOR_0_OUTPUT_0_ID,
+    output_id=OPERATOR_1_INPUT_0_ID,
+)
+
+EDGE_3 = EdgeJSON(
+    input_id=OPERATOR_0_OUTPUT_1_ID,
+    output_id=OPERATOR_2_INPUT_0_ID,
+)
+
+EDGE_4 = EdgeJSON(input_id=OPERATOR_1_INPUT_0_ID, output_id=OPERATOR_1_ID)
+
+EDGE_5 = EdgeJSON(input_id=OPERATOR_2_INPUT_0_ID, output_id=OPERATOR_2_ID)
 
 
 PIPELINE = PipelineJSON(
     id=uuid4(),
-    nodes=[NODE_0, NODE_1],
-    edges=[EDGE_0],
+    operators=[OPERATOR_0, OPERATOR_1, OPERATOR_2],
+    ports=[OPERATOR_0_PORT_0, OPERATOR_0_PORT_1, OPERATOR_1_PORT_0, OPERATOR_2_PORT_0],
+    edges=[EDGE_0, EDGE_1, EDGE_2, EDGE_3, EDGE_4, EDGE_5],
 )
