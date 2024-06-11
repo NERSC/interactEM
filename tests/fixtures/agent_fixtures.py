bf5df89685c87bcef52ef39148be139373e54329
Sam Welborn <swelborn@lbl.gov>
2024-06-11 12:13:30 -0700

    fix tests

--- tests/fixtures/agent_fixtures.py
+++ tests/fixtures/agent_fixtures.py
@@ -4,25 +4,43 @@ import time
 import pytest
 
 from zmglue.agent import Agent
+from zmglue.models import PipelineJSON
 from zmglue.orchestrator import Orchestrator
 from zmglue.pipeline import Pipeline
-from zmglue.models import PipelineJSON
 
 
 @pytest.fixture
-def agent(pipeline: PipelineJSON, orchestrator: Orchestrator):
+def init_agent_with_orchestrator(pipeline: PipelineJSON, orchestrator: Orchestrator):
     agent = Agent()
     agent.pipeline = Pipeline.from_pipeline(pipeline)
     yield agent
 
-    agent.terminate_processes()
-
 
 @pytest.fixture
-def initialized_agent(pipeline: PipelineJSON) -> Agent:
+def init_agent_without_orchestrator(pipeline: PipelineJSON):
     agent = Agent()
     agent.pipeline = Pipeline.from_pipeline(pipeline)
     yield agent
+
+
+@pytest.fixture
+def running_agent_with_orchestrator(pipeline: PipelineJSON, orchestrator: Orchestrator):
+    agent = Agent()
+    agent.pipeline = Pipeline.from_pipeline(pipeline)
+    agent.start()
+    yield agent
+    agent.stop()
+    agent.shutdown()
+
+
+@pytest.fixture
+def running_agent(pipeline: PipelineJSON):
+    agent = Agent()
+    agent.pipeline = Pipeline.from_pipeline(pipeline)
+    agent.start()
+    yield agent
+    agent.stop()
+    agent.shutdown()
 
 
 def run_server_loop(agent):
--- tests/test_agent_without_orchestrator.py
+++ tests/test_agent_without_orchestrator.py
@@ -1,8 +1,12 @@
 from zmglue.agent import Agent
 
 
-def test_start_operators(agent: Agent):
+def test_start_operators(init_agent_without_orchestrator: Agent):
+    agent = init_agent_without_orchestrator
     processes = agent.start_operators()
     assert len(processes) == len(agent.pipeline.operators)
     for process in processes.values():
         assert process.poll() is None  # Check that the process is still running
+
+    agent.stop()
+    agent.shutdown()
