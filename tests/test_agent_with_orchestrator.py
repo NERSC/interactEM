b7483a8f1fd9c8860ea4d3a5af16a6ef5cc16568
Sam Welborn <swelborn@lbl.gov>
2024-06-11 12:13:30 -0700

    fix tests

--- tests/test_agent_with_orchestrator.py
+++ tests/test_agent_with_orchestrator.py
@@ -5,18 +5,21 @@ from zmglue.agent import Agent
 from zmglue.models import PipelineMessage
 
 
-def test_get_pipeline(initialized_agent: Agent, orchestrator, pipeline):
-    response = initialized_agent.get_pipeline()
+def test_get_pipeline(init_agent_with_orchestrator: Agent, pipeline):
+    response = init_agent_with_orchestrator.get_pipeline()
     assert isinstance(response, PipelineMessage)
     assert response.pipeline == pipeline
     assert response.pipeline is not None
 
 
-def test_run(agent: Agent):
+def test_start(init_agent_with_orchestrator: Agent):
     # Start the agent run in a separate thread to allow the test to proceed
-    agent.start()
+    init_agent_with_orchestrator.start()
 
     time.sleep(1)  # Allow some time for the run method to process
 
-    assert len(agent.processes) == len(agent.pipeline.operators)
-    agent.stop()
+    assert len(init_agent_with_orchestrator.processes) == len(
+        init_agent_with_orchestrator.pipeline.operators
+    )
+    init_agent_with_orchestrator.stop()
+    init_agent_with_orchestrator.shutdown()
