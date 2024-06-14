fcff21f79abd69f7c4432fa4b37aae7a592bc340
Sam Welborn <swelborn@lbl.gov>
2024-06-14 17:19:09 -0400

    [tests] don't sleep for that long

--- tests/test_agent_with_orchestrator.py
+++ tests/test_agent_with_orchestrator.py
@@ -16,8 +16,7 @@ def test_start(init_agent_with_orchestrator: Agent):
     # Start the agent run in a separate thread to allow the test to proceed
     init_agent_with_orchestrator.start()
 
-    time.sleep(1)  # Allow some time for the run method to process
-
+    time.sleep(0.5)  # Allow some time for the run method to process
     assert len(init_agent_with_orchestrator.processes) == len(
         init_agent_with_orchestrator.pipeline.operators
     )
