1c08b2901a7822c50888f8151124338f5eec8d1d
Chris Harris <cjh@lbl.gov>
2024-06-26 15:06:06 +0000

    Allow agent and orchestrator inferface to be configured

--- zmglue/config.py
+++ zmglue/config.py
@@ -8,7 +8,9 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
     AGENT_PORT: int
+    AGENT_INTERFACE: str | None
     ORCHESTRATOR_PORT: int
+    ORCHESTRATOR_INTERFACE: str | None
     CONTAINER_INTERFACE: str | None
 
 
