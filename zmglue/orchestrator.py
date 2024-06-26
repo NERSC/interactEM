b8563bd6ad17491cbe52217ea9c06c5fd2b94fbe
Chris Harris <cjh@lbl.gov>
2024-06-26 15:06:06 +0000

    Allow agent and orchestrator inferface to be configured

--- zmglue/orchestrator.py
+++ zmglue/orchestrator.py
@@ -33,7 +33,7 @@ from zmglue.zsocket import Socket, SocketInfo
 logger = get_logger("forchestrator", "DEBUG")
 
 DEFAULT_ORCHESTRATOR_SOCKET_ADDRESS: str = (
-    f"tcp://?interface=*&hostname=localhost&port={cfg.ORCHESTRATOR_PORT}"
+    f"tcp://?interface={cfg.ORCHESTRATOR_INTERFACE}&hostname=localhost&port={cfg.ORCHESTRATOR_PORT}"
 )
 DEFAULT_ORCHESTRATOR_URI = URI(
     id=uuid4(),
