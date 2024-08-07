2863d916af0e10fc125c745d3b0133bf32e96492
Sam Welborn <swelborn@lbl.gov>
2024-08-07 20:35:35 +0000

    update orchestrator to latest nats.py services API

--- zmglue/nats/orchestrator_service.py
+++ zmglue/nats/orchestrator_service.py
@@ -3,8 +3,11 @@ import contextlib
 import signal
 from uuid import uuid4
 
+import nats
 from nats import micro
 from nats.aio.client import Client as NATS
+from nats.micro.request import Request
+from nats.micro.service import GroupConfig, Service, ServiceConfig
 from pydantic import ValidationError
 
 from zmglue.fixtures import PIPELINE
@@ -32,7 +35,6 @@ DEFAULT_ORCHESTRATOR_URI = URI(
     query={"address": [DEFAULT_NATS_ADDRESS]},
 )
 
-
 class Orchestrator:
     def __init__(self):
         self.nc = NATS()
@@ -43,44 +45,46 @@ class Orchestrator:
     async def connect(self):
         await self.nc.connect(servers=[DEFAULT_NATS_ADDRESS])
 
-    async def handle_pipeline_request(self, msg: micro.Request):
+    async def handle_pipeline_request(self, msg: Request):
         try:
-            PipelineMessage.model_validate_json(msg.data())
+            PipelineMessage.model_validate_json(msg.data)
         except ValidationError:
             # TODO: Correct error code/message
-            await msg.respond_error(code=400, description="Invalid pipeline message.")
+            await msg.respond_error(code="400", description="Invalid pipeline message.")
             return
         await msg.respond(
             PipelineMessage(pipeline=self.pipeline.to_json()).model_dump_json().encode()
         )
 
-    async def handle_uri_update_request(self, msg: micro.Request):
+    async def handle_uri_update_request(self, msg: Request):
         try:
-            model = URIUpdateMessage.model_validate_json(msg.data())
+            model = URIUpdateMessage.model_validate_json(msg.data)
         except ValidationError:
             # TODO: Correct error code/message
-            await msg.respond_error(code=400, description="Invalid URI update message.")
+            await msg.respond_error(
+                code="400", description="Invalid URI update message."
+            )
             return
         logger.info(f"Received URI update request from {model.id}.")
         self.pipeline.update_uri(model)
-        await msg.respond(msg.data())
+        await msg.respond(msg.data)
 
-    async def handle_uri_connect_request(self, msg: micro.Request):
-        model = URIConnectMessage.model_validate_json(msg.data())
+    async def handle_uri_connect_request(self, msg: Request):
+        model = URIConnectMessage.model_validate_json(msg.data)
         uris = self.pipeline.get_connections(model)
         await msg.respond(
             URIConnectResponseMessage(connections=uris).model_dump_json().encode()
         )
 
-    async def handle_put_pipeline_node(self, msg: micro.Request):
+    async def handle_put_pipeline_node(self, msg: Request):
         try:
-            model = PutPipelineNodeMessage.model_validate_json(msg.data())
+            model = PutPipelineNodeMessage.model_validate_json(msg.data)
         except ValidationError as e:
             # TODO: Correct error code/message
-            await msg.respond_error(code=400, description=str(e))
+            await msg.respond_error(code="400", description=str(e))
             return
         self.pipeline.put_node(model)
-        await msg.respond(msg.data())
+        await msg.respond(msg.data)
 
     async def run(self):
         logger.info("Starting orchestrator...")
@@ -90,23 +94,27 @@ class Orchestrator:
             self.loop.add_signal_handler(sig, lambda *_: self.quit_event.set())
         async with contextlib.AsyncExitStack() as stack:
             logger.info("Connecting to NATS...")
-            await self.nc.connect("nats://localhost:4222")
-            logger.info("Connected to NATS.")
+            connect_address = "nats://localhost:4222"
+            self.nc = await stack.enter_async_context(
+                await nats.connect(connect_address)
+            )
+            logger.info(f"Connected to NATS at {connect_address}")
             stack.push_async_callback(self.nc.close)
-
-            service = await stack.enter_async_context(
-                micro.add_service(
+            service: Service = await stack.enter_async_context(
+                await micro.add_service(
                     self.nc,
-                    name="orchestrator-service",
-                    version="0.0.1",
-                    description="Orchestrator service",
+                    config=ServiceConfig(
+                        name="orchestrator-service",
+                        version="0.0.1",
+                        description="Orchestrator service",
+                    ),
                 )
             )
             logger.info("Orchestrator service created.")
 
-            orchestrator = service.add_group("orchestrator")
+            orchestrator = service.add_group(GroupConfig(name="orchestrator"))
 
-            pipeline = orchestrator.add_group(name="pipeline")
+            pipeline = orchestrator.add_group(GroupConfig(name="pipeline"))
             await pipeline.add_endpoint(
                 name="get",
                 handler=self.handle_pipeline_request,
@@ -116,7 +124,7 @@ class Orchestrator:
                 handler=self.handle_put_pipeline_node,
             )
 
-            uri_grp = orchestrator.add_group("uri")
+            uri_grp = orchestrator.add_group(GroupConfig(name="uri"))
             await uri_grp.add_endpoint(
                 name="get",
                 handler=self.handle_uri_connect_request,
