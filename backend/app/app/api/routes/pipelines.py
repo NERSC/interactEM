d446210add0dcdd4da7cc2c083014260a414c32c
Sam Welborn <swelborn@lbl.gov>
2024-08-11 22:18:43 -0400

    add a pipelines/{id}/run endpoint to test jetstream

--- backend/app/app/api/routes/pipelines.py
+++ backend/app/app/api/routes/pipelines.py
@@ -1,10 +1,12 @@
 import uuid
 from typing import Any
 
+from core.logger import get_logger
 from fastapi import APIRouter, HTTPException
 from sqlmodel import func, select
 
 from app.api.deps import CurrentUser, SessionDep
+from app.events.producer import publish_pipeline_run_event
 from app.models import (
     Message,
     Pipeline,
@@ -13,6 +15,7 @@ from app.models import (
     PipelinesPublic,
 )
 
+logger = get_logger(__name__)
 router = APIRouter()
 
 
@@ -67,7 +70,9 @@ def create_pipeline(
     """
     Create new pipeline.
     """
-    pipeline = Pipeline.model_validate(pipeline_in, update={"owner_id": current_user.id})
+    pipeline = Pipeline.model_validate(
+        pipeline_in, update={"owner_id": current_user.id}
+    )
     session.add(pipeline)
     session.commit()
     session.refresh(pipeline)
@@ -89,3 +94,30 @@ def delete_pipeline(
     session.delete(pipeline)
     session.commit()
     return Message(message="Pipeline deleted successfully")
+
+
+@router.post("/{id}/run", response_model=PipelinePublic)
+async def run_pipeline(
+    session: SessionDep, current_user: CurrentUser, id: uuid.UUID
+) -> PipelinePublic:
+    """
+    Run a pipeline.
+    """
+    pipeline = session.get(Pipeline, id)
+    if not pipeline:
+        raise HTTPException(status_code=404, detail="Pipeline not found")
+    if not current_user.is_superuser and (pipeline.owner_id != current_user.id):
+        raise HTTPException(status_code=400, detail="Not enough permissions")
+
+    pipeline = PipelinePublic.model_validate(pipeline)
+
+    try:
+        await publish_pipeline_run_event(pipeline)
+        logger.info(f"Sent publish pipeline run event for pipeline {pipeline.id}")
+    except Exception as e:
+        logger.error(
+            f"Failed to publish pipeline run event for pipeline {pipeline.id}: {e}"
+        )
+        raise HTTPException(status_code=500, detail="Failed to run pipeline")
+
+    return pipeline
