7de06857739b64c6f69baf0a12f6283963f42102
Sam Welborn <swelborn@lbl.gov>
2024-08-15 08:09:32 -0400

    enable jetstream on producer
    - copies models/constants/logger/pipeline into ./core in ./backend
    - adds core package
    - adds core package to app
    - starts up jetstream on app startup, adds pipelines stream
     - if pipelines stream already there, it will update it with new configuration
    - adds logic to publish on the stream from an event
    
    [reorder] move pipeline
    
    [reorder] move config
    
    [reorder] move fixtures
    
    [reorder] move logger
    
    [reorder] move models

--- backend/app/app/events/producer.py
+++ backend/app/app/events/producer.py
@@ -1,37 +1,74 @@
 import logging
 
 import nats
+import nats.errors
+from core.constants import (
+    STREAM_PIPELINES,
+    SUBJECT_PIPELINES_RUN,
+)
+from nats.aio.client import Client as NATSClient
+from nats.js import JetStreamContext
+from nats.js.api import StreamConfig
+from nats.js.errors import BadRequestError
+from pydantic import BaseModel
+from sqlmodel import SQLModel
 
 from ..core.config import settings
-from ..models import Pipeline
+from ..models import PipelinePublic
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-producer = None
-
-SUBJECT_PIPELINES_EVENT = "pipelines"
+nats_client: NATSClient | None = None
+nats_jetstream: JetStreamContext | None = None
 
 
 async def start():
-    global producer
+    global nats_client
+    global nats_jetstream
     logger.info(f"Connecting to NATS server: {settings.NATS_SERVER_ADDRESS}")
-    producer = await nats.connect(settings.NATS_SERVER_ADDRESS.unicode_string())
+    nats_client = await nats.connect(settings.NATS_SERVER_ADDRESS.unicode_string())
+    nats_jetstream = nats_client.jetstream()
+    pipeline_cfg = StreamConfig(
+        name=STREAM_PIPELINES,
+        description="A stream for pipeline messages",
+        subjects=[f"{STREAM_PIPELINES}.>"],
+    )
+    try:
+        pipeline_stream_info = await nats_jetstream.add_stream(config=pipeline_cfg)
+    except BadRequestError as e:
+        if e.err_code == 10058: # Stream already exists
+            pipeline_stream_info = await nats_jetstream.update_stream(config=pipeline_cfg)
+        else:
+            raise
+    logger.info(f"Stream information: {pipeline_stream_info}")
 
 
 async def stop():
-    if producer:
-        await producer.close()
+    if nats_client:
+        await nats_client.close()
 
 
-async def publish_pipeline_event(event: Pipeline) -> None:
-    if producer is None:
+async def publish_jetstream_event(
+    stream: str,
+    subject: str,
+    event: BaseModel | SQLModel,
+) -> None:
+    if nats_jetstream is None:
         raise Exception("Producer has not been initialized")
 
     try:
-        await producer.publish(
-            SUBJECT_PIPELINES_EVENT, event.model_dump_json().encode()
+        await nats_jetstream.publish(
+            subject=subject,
+            payload=event.model_dump_json().encode(),
+            timeout=None,
+            stream=stream,
+            headers=None,
         )
     except:  # noqa
-        logger.exception(f"Exception send on topic: {SUBJECT_PIPELINES_EVENT}")
+        logger.exception(f"Exception send on subject: {subject}")
+
+
+async def publish_pipeline_run_event(event: PipelinePublic) -> None:
+    await publish_jetstream_event(STREAM_PIPELINES, SUBJECT_PIPELINES_RUN, event)
