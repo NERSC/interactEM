5852b99cf9f5e7cb1755b9b1c0517e7ae311b574
Sam Welborn <swelborn@lbl.gov>
2024-08-20 17:27:53 -0400

    run orchestrator from run.py

--- backend/orchestrator/orchestrator/pull.py
+++ backend/orchestrator/orchestrator/pull.py
@@ -181,7 +181,3 @@ async def main():
 
     while True:
         await asyncio.sleep(1)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
--- backend/orchestrator/run.py
+++ backend/orchestrator/run.py
@@ -1,1 +1,6 @@
+import asyncio
 
+from orchestrator.pull import main
+
+if __name__ == "__main__":
+    asyncio.run(main())
