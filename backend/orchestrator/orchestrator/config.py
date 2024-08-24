b44a0c2cc124c43e8217059f4d055ce690cd5c4c
Sam Welborn <swelborn@lbl.gov>
2024-08-24 06:16:01 -0400

    wss scheme not supported by pydantic (yet)
    made PR to pydantic to allow NatsDsn to have wss, but for now we have to do this

--- backend/orchestrator/orchestrator/config.py
+++ backend/orchestrator/orchestrator/config.py
@@ -1,10 +1,10 @@
-from pydantic import Field, NatsDsn
+from pydantic import Field, NatsDsn, WebsocketUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=None)
-    NATS_SERVER_URL: NatsDsn = Field(default="nats://localhost:4222")
+    NATS_SERVER_URL: NatsDsn | WebsocketUrl = Field(default="nats://localhost:4222")
 
 
 cfg = Settings()
