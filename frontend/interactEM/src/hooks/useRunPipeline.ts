bcc14c9b51a1ec0fae64eec3d3e7f12862af1d19
Sam Welborn <swelborn@lbl.gov>
2024-10-25 06:00:33 -0700

    [frontend] move mutations to hooks

---
+++ frontend/interactEM/src/hooks/useRunPipeline.ts
@@ -0,0 +1,17 @@
+import { useMutation } from "@tanstack/react-query"
+import { runPipelineMutation } from "../client/@tanstack/react-query.gen"
+
+export const useRunPipeline = () => {
+  return useMutation({
+    ...runPipelineMutation(),
+    onError: (error) => {
+      console.error("Error running pipeline:", error)
+    },
+    onSuccess: (data) => {
+      console.log("Pipeline launched:", data)
+    },
+    onSettled: () => {
+      console.log("Pipeline launch settled.")
+    },
+  })
+}
