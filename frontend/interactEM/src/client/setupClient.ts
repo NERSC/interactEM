c66b4ba95e8793cb1ba834b5962661100e5b0c9e
Sam Welborn <swelborn@lbl.gov>
2024-10-29 10:43:15 -0400

    [frontend] separate config to one place

--- frontend/interactEM/src/client/setupClient.ts
+++ frontend/interactEM/src/client/setupClient.ts
@@ -1,12 +1,12 @@
 import { client } from "./index"
+import clientConfig from "../config"
 
 client.setConfig({
-  baseURL:
-    import.meta.env.VITE_REACT_APP_API_BASE_URL || "http://localhost:80/",
+  baseURL: clientConfig.API_BASE_URL || "http://localhost:80/",
 })
 
 client.instance.interceptors.request.use((config) => {
-  const token = import.meta.env.VITE_REACT_APP_API_TOKEN
+  const token = clientConfig.API_TOKEN
 
   if (token) {
     config.headers.set("Authorization", `Bearer ${token}`)
