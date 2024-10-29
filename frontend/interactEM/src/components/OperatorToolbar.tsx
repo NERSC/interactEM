3d4ae043b2afb063412570eea3915da780bcd7b7
Sam Welborn <swelborn@lbl.gov>
2024-10-29 11:18:02 -0400

    [frontend] node toolbar + styling

---
+++ frontend/interactEM/src/components/OperatorToolbar.tsx
@@ -0,0 +1,49 @@
+import type React from "react"
+import { IconButton, Tooltip } from "@mui/material"
+import InfoIcon from "@mui/icons-material/Info"
+import type { OperatorParameter } from "../operators"
+import ParametersButton from "./ParametersButton"
+
+interface OperatorToolbarProps {
+  id: string
+  image: string
+  parameters?: OperatorParameter[]
+  nodeRef: React.RefObject<HTMLDivElement>
+}
+
+const OperatorToolbar: React.FC<OperatorToolbarProps> = ({
+  id,
+  image,
+  parameters,
+  nodeRef,
+}) => {
+  return (
+    <div className="operator-toolbar">
+      <div className="operator-icons">
+        {parameters && (
+          <ParametersButton
+            operatorID={id}
+            parameters={parameters}
+            nodeRef={nodeRef}
+          />
+        )}
+
+        <Tooltip
+          title={
+            <div>
+              <div>ID: {id}</div>
+              <div>Image: {image}</div>
+            </div>
+          }
+          placement="top"
+        >
+          <IconButton size="small" aria-label="Info">
+            <InfoIcon fontSize="small" />
+          </IconButton>
+        </Tooltip>
+      </div>
+    </div>
+  )
+}
+
+export default OperatorToolbar
