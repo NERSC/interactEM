898697cc3c0ddeba70b4f567ca99955f482bc59b
Chris Harris <cjh@lbl.gov>
2024-11-06 20:05:18 +0000

    Add image node to display image data

--- frontend/interactEM/src/constants/nats.ts
+++ frontend/interactEM/src/constants/nats.ts
@@ -2,3 +2,4 @@ export const PARAMETERS_BUCKET = "parameters"
 export const PARAMETERS_STREAM = PARAMETERS_BUCKET
 export const PARAMETERS_QUERYKEY = "parameterValue"
 export const PARAMETERS_UPDATE_STREAM = `${PARAMETERS_STREAM}.update`
+export const IMAGES_STREAM = "images"
