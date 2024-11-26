5cc272dfa656923b0e348bd08a23a53e3dc9b490
Sam Welborn <swelborn@lbl.gov>
2024-11-25 23:10:42 -0500

    simplify Containerfiles and improve build script
    - we use interactem/operator where we can
    - I think we will eventually have this as pip installable, but this works to reduce bloat for now.
    - note: maybe we should use FROM interactem/operator and copy in the packages directly?

--- operators/build_all.sh
+++ operators/build_all.sh
@@ -3,33 +3,54 @@
 set -e
 
 # Get the directory of this script
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR=$(dirname "$0")
+ROOT_DIR=$(git rev-parse --show-toplevel)
 
-# Define the operators directory relative to this script
-OPERATORS_DIR="$SCRIPT_DIR/../../operators"
+# Build base containers
+$ROOT_DIR/docker/build.sh
+if [ $? -ne 0 ]; then
+    echo "Failed to build base containers."
+    exit 1
+fi
 
-BACKEND_DIR="$SCRIPT_DIR/../../"
+build_dirs=(
+    "background-subtract"
+    "data-replay"
+    "electron-count-save"
+    "error"
+    "image-display"
+    "random-image"
+    "sparse-frame-image-converter"
+)
 
-# Define the containerfiles directory
-CONTAINERFILES_DIR="$OPERATORS_DIR/containerfiles"
+# Function to check if a directory is in the build_dirs array
+function is_included {
+    local name="$1"
+    local dir
+    for dir in "${build_dirs[@]}"; do
+        if [[ "$dir" == "$name" ]]; then
+            return 0  # Found
+        fi
+    done
+    return 1  # Not found
+}
 
-# Initialize an array to hold process IDs (PIDs) of background jobs
 declare -a pids
-
-# Counter for PIDs array
 i=0
-
-# Loop over each directory in the containerfiles directory
-for dir in "$CONTAINERFILES_DIR"/*; do
+for dir in "$SCRIPT_DIR"/*; do
     if [ -d "$dir" ]; then
         op_name=$(basename "$dir")
+        # Check if the directory is in the list to build
+        if ! is_included "$op_name"; then
+            echo "Skipping $op_name as it's not in the build list"
+            continue
+        fi
         containerfile="$dir/Containerfile"
         if [ -f "$containerfile" ]; then
             echo "Found Containerfile for $op_name"
-
             echo "Building image for $op_name"
             # Start the build in background and collect its PID
-            podman build -t "interactem/$op_name" "$BACKEND_DIR" -f $containerfile &
+            podman build -t "interactem/$op_name" -f "$containerfile" "$dir" &
             pids[$i]=$!
             ((i++))
         else
