4c9885326f8a128a553cbd66a50f89db4094d71e
Sam Welborn <swelborn@lbl.gov>
2024-12-04 17:02:58 -0500

    mount local repo
    - added these utilities to avoid having to rebuild containers for each change we make in the backend

--- backend/agent/agent/config.py
+++ backend/agent/agent/config.py
@@ -3,13 +3,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
     LOCAL: bool = True
     DOCKER_COMPATIBILITY_MODE: bool = False
     PODMAN_SERVICE_URI: str | None = None
     NATS_SERVER_URL: NatsDsn = Field(default="nats://localhost:4222")
     NATS_SERVER_URL_IN_CONTAINER: NatsDsn = Field(default="nats://nats1:4222")
     AGENT_MACHINE_NAME: str | None = None
+    MOUNT_LOCAL_REPO: bool = False
 
 
 cfg = Settings()
