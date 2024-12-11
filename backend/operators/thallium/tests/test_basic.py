fbb28f937678da9c64dc5120a5b46ba59acff1cd
Sam Welborn <swelborn@lbl.gov>
2024-12-11 10:32:54 -0500

    basic thallium bindings

--- backend/operators/thallium/tests/test_basic.py
+++ backend/operators/thallium/tests/test_basic.py
@@ -1,4 +1,19 @@
-import nanobind_example as m
+import re
 
-def test_add():
-    assert m.add(1, 2) == 3
+import thallium
+
+
+def test_queue_provider():
+    qprov = thallium.QueueProvider("na+sm", 1)
+    address = qprov.get_address()
+    pattern = re.compile(r"^na\+sm://\d+-\d+$")
+    assert pattern.match(
+        address
+    ), f"Address '{address}' does not match the expected format 'na+sm://XXX-YYY'"
+
+    qclient = thallium.QueueClient("na+sm", address, 1)
+    qclient.push("Hello")
+    assert qprov.pull() == "Hello"
+
+    qclient.push_rdma("Hello2")
+    assert qprov.pull() == "Hello2"
