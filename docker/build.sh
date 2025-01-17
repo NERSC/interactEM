7d7707650924b637c936bf4aa86e67a31ec1b018
Chris Harris <cjh@lbl.gov>
2025-01-17 16:35:43 +0000

    Build base images using podman

--- docker/build.sh
+++ docker/build.sh
@@ -5,13 +5,13 @@ ROOT_DIR=$(git rev-parse --show-toplevel)
 TAG=$(git rev-parse --short=6 HEAD)
 ORG=${1:-interactem}
 
-docker build --platform=linux/amd64,linux/arm64 -t interactem/interactem:$TAG -t interactem/interactem:latest -f $SCRIPT_DIR/Containerfile.base $ROOT_DIR/backend
+podman build --platform=linux/amd64,linux/arm64 -t interactem/interactem:$TAG -t interactem/interactem:latest -f $SCRIPT_DIR/Containerfile.base $ROOT_DIR/backend
 if [ $? -ne 0 ]; then
     echo "Failed to build interactem/interactem"
     exit 1
 fi
 
-docker build --platform=linux/amd64,linux/arm64 -t $ORG/operator:$TAG -f $SCRIPT_DIR/Containerfile.operator $ROOT_DIR/backend
+podman build --platform=linux/amd64,linux/arm64 -t $ORG/operator:$TAG -f $SCRIPT_DIR/Containerfile.operator $ROOT_DIR/backend
 if [ $? -ne 0 ]; then
     echo "Failed to build interactem/operator"
     exit 1
