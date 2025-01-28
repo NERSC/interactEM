9c05d124be1f259a4e2e8083505be75ad4a9c7c4
Sam Welborn <swelborn@lbl.gov>
2025-01-28 16:56:51 -0500

    config -> .env.development

--- frontend/interactEM/src/config/index.ts
+++ frontend/interactEM/src/config/index.ts
@@ -1,11 +1,13 @@
 interface Config {
   NATS_SERVER_URL: string
   API_BASE_URL: string
+  INTERACTEM_ADMIN_TOKEN: string
 }
 
 const config: Config = {
   NATS_SERVER_URL: import.meta.env.VITE_NATS_SERVER_URL,
   API_BASE_URL: import.meta.env.VITE_REACT_APP_API_BASE_URL,
+  INTERACTEM_ADMIN_TOKEN: import.meta.env.VITE_INTERACTEM_ADMIN_TOKEN,
 }
 
 export default config
