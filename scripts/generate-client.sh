7e7aa7765755d7d93050f6c3d68931fd3b352228
Sam Welborn <swelborn@lbl.gov>
2025-02-05 09:11:24 -0500

    fix generate-client (again)
    - hopefully this will be the last time we have to change this script :)

--- scripts/generate-client.sh
+++ scripts/generate-client.sh
@@ -5,8 +5,12 @@ set -x
 
 # activate backend/app environment using poetry shell command before running
 THIS_DIR=$(dirname "$0")
-cd $THIS_DIR/..
-python -c "import interactem.app.main; import json; print(json.dumps(interactem.app.main.app.openapi()))" > openapi.json
+ROOT_DIR=$(realpath $THIS_DIR/..)
+cd $ROOT_DIR
+poetry run -P backend/app -- \
+    python -c "import interactem.app.main; import json; print(json.dumps(interactem.app.main.app.openapi()))" \
+    > $ROOT_DIR/openapi.json
+cd $ROOT_DIR
 mv openapi.json frontend/interactEM/openapi.json
 cd frontend/interactEM
 npm run generate-client
