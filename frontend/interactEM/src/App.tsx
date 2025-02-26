0dbc6df7079fa2dde43ce033c1e5498188cbaf1c
Sam Welborn <swelborn@lbl.gov>
2025-02-26 16:05:17 -0500

    add toast to page instead of app

--- frontend/interactEM/src/App.tsx
+++ frontend/interactEM/src/App.tsx
@@ -1,21 +1,9 @@
-import { Flip, ToastContainer } from "react-toastify"
 import InteractEM from "./pages/interactem"
 
 export default function App() {
   return (
     <>
       <InteractEM authMode="internal" />
-      <ToastContainer
-        position="top-left"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        transition={Flip}
-      />
     </>
   )
 }
