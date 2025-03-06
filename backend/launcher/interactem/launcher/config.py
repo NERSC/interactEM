308c5075adecc6aae0c24b4d038edb7606897bb3
swelborn <55868530+swelborn@users.noreply.github.com>
2025-03-06 12:22:22 -0500

    go to envfile dir instead of using dotenv run

--- backend/launcher/interactem/launcher/config.py
+++ backend/launcher/interactem/launcher/config.py
@@ -10,6 +10,7 @@ class Settings(BaseSettings):
     SFAPI_KEY_PATH: Path = Path("/secrets/sfapi.pem")
     CONDA_ENV: Path | str
     ENV_FILE_PATH: Path
+    ENV_FILE_DIR: Path | None = None
     SFAPI_ACCOUNT: str
     SFAPI_QOS: str
 
@@ -18,6 +19,11 @@ class Settings(BaseSettings):
         self.SFAPI_KEY_PATH = self.SFAPI_KEY_PATH.expanduser().resolve()
         if not self.SFAPI_KEY_PATH.is_file():
             raise ValueError(f"File not found: {self.SFAPI_KEY_PATH}")
+        return self
+
+    @model_validator(mode="after")
+    def env_file_parent(self) -> "Settings":
+        self.ENV_FILE_DIR = self.ENV_FILE_PATH.parent
         return self
 
 
