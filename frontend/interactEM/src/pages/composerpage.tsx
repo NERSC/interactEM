eba0d3f6e83239b46d7017653c39dff6f5282a14
swelborn <55868530+swelborn@users.noreply.github.com>
2025-04-21 18:00:35 -0400

    move pipeline list into pipeline HUD
    - also move FAB to pipeline hud
    
    - display current revision, no last updated.

--- frontend/interactEM/src/pages/composerpage.tsx
+++ frontend/interactEM/src/pages/composerpage.tsx
@@ -1,11 +1,8 @@
-import ListIcon from "@mui/icons-material/List"
-import { Fab, Stack } from "@mui/material"
-import { useCallback, useState } from "react"
+import { Stack } from "@mui/material"
 import AgentsArray from "../components/agentsarray"
 import ComposerPipelineFlow from "../components/composerpipelineflow"
-import { CurrentPipelineDisplay } from "../components/currentpipelinedisplay"
 import { OperatorMenu } from "../components/operatormenu"
-import { PipelineList } from "../components/pipelinelist"
+import { PipelineHud } from "../components/pipelinehud"
 import { useActivePipeline } from "../hooks/useActivePipeline"
 import { useAgents } from "../hooks/useAgents"
 import useOperators from "../hooks/useOperators"
@@ -14,17 +11,8 @@ import { usePipelineStore } from "../stores"
 export default function ComposerPage() {
   const { operators } = useOperators()
   const { agents } = useAgents()
-  const [isPipelineDrawerOpen, setIsPipelineDrawerOpen] = useState(false)
   const { revision } = useActivePipeline()
   const { currentPipelineId } = usePipelineStore()
-
-  const handleTogglePipelineDrawer = () => {
-    setIsPipelineDrawerOpen(!isPipelineDrawerOpen)
-  }
-
-  const handleClosePipelineDrawer = useCallback(() => {
-    setIsPipelineDrawerOpen(false)
-  }, [])
 
   return (
     <div className="composer-page">
@@ -40,26 +28,10 @@ export default function ComposerPage() {
           alignItems: "center",
         }}
       >
-        {/* Pipeline List Drawer Button */}
-        <Fab
-          color="primary"
-          aria-label="open pipeline list"
-          onClick={handleTogglePipelineDrawer}
-          size="small"
-        >
-          <ListIcon />
-        </Fab>
-
-        {/* Current Pipeline Display */}
-        <CurrentPipelineDisplay />
+        {/* Self-contained Pipeline HUD with integrated pipeline drawer */}
+        <PipelineHud />
         <AgentsArray agents={agents} />
       </Stack>
-
-      {/* Pipeline List Drawer */}
-      <PipelineList
-        open={isPipelineDrawerOpen}
-        onClose={handleClosePipelineDrawer}
-      />
 
       {/* Main Flow Area */}
       <div className="composer-flow">
