a294c6929617e75b5cd2b502fdb04946c1c5d692
swelborn <55868530+swelborn@users.noreply.github.com>
2025-08-14 13:56:29 -0400

    remove utils router

--- backend/app/interactem/app/api/main.py
+++ backend/app/interactem/app/api/main.py
@@ -7,13 +7,11 @@ from interactem.app.api.routes import (
     operators,
     pipelines,
     users,
-    utils,
 )
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(pipelines.router, prefix="/pipelines", tags=["pipelines"])
 api_router.include_router(
     deployments.router, prefix="/deployments", tags=["deployments"]
