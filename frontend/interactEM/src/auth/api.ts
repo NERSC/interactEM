64e90493545ae2f377df608dd16f01f408b4a9ec
swelborn <swelborn@lbl.gov>
2025-09-24 16:51:08 -0400

    use store for token

--- frontend/interactEM/src/auth/api.ts
+++ frontend/interactEM/src/auth/api.ts
@@ -1,5 +1,6 @@
 import { QueryClient } from "@tanstack/react-query"
 import { AUTH_QUERY_KEYS } from "../constants/tanstack"
+import { authStore } from "../stores"
 
 const queryClient = new QueryClient()
 
@@ -12,7 +13,9 @@ export async function loginInteractem(
   external_token: string,
 ): Promise<LoginResult> {
   try {
-    queryClient.setQueryData(AUTH_QUERY_KEYS.externalToken, external_token)
+    console.log("Logging in to InteractEM with external token...")
+    authStore.getState().setExternalToken(external_token)
+
     // Trigger a refresh of the internal auth
     await queryClient.invalidateQueries({
       queryKey: AUTH_QUERY_KEYS.internalToken,
