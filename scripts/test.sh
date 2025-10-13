82f2bc3be0b6ec2a98738f25e381570532ef0e15
swelborn <swelborn@lbl.gov>
2025-10-13 18:59:47 -0400

    test-all script

---
+++ scripts/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
+cd $GIT_ROOT_DIR
+
+export NATS_CREDS_FILE=$(pwd)/nats-conf/out_jwt/backend.creds
+
+cd $GIT_ROOT_DIR/backend/core
+poetry install
+poetry env info
+poetry run pytest 
+
+cd $GIT_ROOT_DIR/backend/orchestrator
+poetry install
+poetry env info
+poetry run pytest
+
+cd $GIT_ROOT_DIR/operators/distiller-streaming
+poetry install
+poetry env info
+poetry run pytest
