94cb0d4ea1ed7d9bec64b97db334705e38d892e8
swelborn <swelborn@lbl.gov>
2025-10-27 19:03:01 -0400

    add more to update-locks script

--- scripts/update-locks.sh
+++ scripts/update-locks.sh
@@ -4,25 +4,46 @@ GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
 cd $GIT_ROOT_DIR
 
 cd backend/core
+uv venv .venv --clear && source .venv/bin/activate
 poetry lock
+poetry install
 
 cd ../sfapi_models
+uv venv .venv --clear && source .venv/bin/activate
 poetry lock
+poetry install
 
 cd ../app
+uv venv .venv --clear && source .venv/bin/activate
 poetry lock
+poetry install
 
 cd ../agent
+uv venv .venv --clear && source .venv/bin/activate
 poetry lock
+poetry install
 
 cd ../orchestrator
+uv venv .venv --clear && source .venv/bin/activate
 poetry lock
+poetry install
 
 cd ../operators
+uv venv .venv --clear && source .venv/bin/activate
 poetry lock
+poetry install
 
 cd ../metrics
+uv venv .venv --clear && source .venv/bin/activate
 poetry lock
+poetry install
 
 cd ../launcher
+uv venv .venv --clear && source .venv/bin/activate
 poetry lock
+poetry install
+
+cd $GIT_ROOT_DIR
+uv venv .venv --clear && source .venv/bin/activate
+poetry lock
+poetry install
