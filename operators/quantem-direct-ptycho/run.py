92c2fbca9a1e657ed24249198d01078e8e42653e
Peter Ercius <percius@lbl.gov>
2025-12-09 11:12:43 -0800

    add parameters needed for the calculation. Some of the should be from microscope metadata, but we need these here for now.

--- operators/quantem-direct-ptycho/run.py
+++ operators/quantem-direct-ptycho/run.py
@@ -85,8 +85,14 @@ def py4dstem_parallax(
 
     logger.info(f"Scan {scan_number}: Calculating ptycho images.")
 
-    probe_semiangle = 25
-    energy = 300e3
+    # Calculation parameters
+    probe_semiangle = float(parameters.get("probe_semiangle", 25.0))
+    energy = int(parameters.get("accelerating_voltage", 300e3))
+    probe_step_size = float(parameters.get("probe_step_size", 0.1)) # test data set: 0.14383155 nm
+    initial_defocus_nm = float(parameters.get("initial_defocus", 0)) # in nanometers
+    initial_defocus_A = initial_defocus_nm * 10 # convert to Angstroms
+    diffraction_rotation_angle = float(parameters.get("diffraction_rotation_angle", 0)) # in degrees
+    rotation_angle = diffraction_rotation_angle * np.pi / 180  # convert to radians
 
     logger.info("densify")
     data = accumulator.to_dense()[:,:-1,:,:]  ## remove last row and column to make it even sized
@@ -105,8 +111,8 @@ def py4dstem_parallax(
     dset.sampling[3] = probe_semiangle / probe_R
     dset.units[2:] = ["mrad", "mrad"]
 
-    dset.sampling[0] = 0.14383155 * 10 ## this has to be Anggstrom for quantem
-    dset.sampling[1] = 0.14383155 * 10
+    dset.sampling[0] = probe_step_size * 10 ## convert to be Anggstrom for quantem. distiller will give nanometers.
+    dset.sampling[1] = probe_step_size * 10
     dset.units[0:2] = ["A", "A"]
 
     logger.info(f"Scan {scan_number}: Start direct ptycho")
@@ -116,9 +122,9 @@ def py4dstem_parallax(
                             energy=energy,
                             semiangle_cutoff=probe_semiangle,
                             device="cpu",
-                            aberration_coefs={'C10':0},
+                            aberration_coefs={'C10':initial_defocus_A},
                             max_batch_size=10,
-                            rotation_angle=0, # need radians
+                            rotation_angle=rotation_angle, # need radians
                             )
         
         logger.info(f"Scan {scan_number}: Fit hyperparameters")
