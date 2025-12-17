fd944cb01bf7a74dbf7e0f6257c058af230ba5af
swelborn <swelborn@lbl.gov>
2025-12-17 18:06:22 -0500

    add hdf locking env var to startup script

--- backend/launcher/tests/expected_script.sh
+++ backend/launcher/tests/expected_script.sh
@@ -14,5 +14,6 @@
 #SBATCH --nodes=2
 #SBATCH --exclusive
 
+export HDF5_USE_FILE_LOCKING=FALSE
 cd /path/to/.env
 srun --nodes=2 --ntasks-per-node=1 uv run --project /path/to/interactEM/backend/agent interactem-agent
