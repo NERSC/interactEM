6bb10ea6a28ec14a7fb9609902c1303fe769cc66
Peter Ercius <percius@lbl.gov>
2026-01-21 14:37:26 -0800

    missed an import

--- operators/quantem-direct-ptycho/run.py
+++ operators/quantem-direct-ptycho/run.py
@@ -8,6 +8,7 @@ from distiller_streaming.accumulator import FrameAccumulator
 from distiller_streaming.models import BatchedFrames
 from quantem.core.utils.diffractive_imaging_utils import fit_probe_circle
 from quantem.diffractive_imaging.direct_ptychography import DirectPtychography
+from quantem.diffractive_imaging.direct_ptychography import OptimizationParameter
 
 from interactem.core.logger import get_logger
 from interactem.core.models.messages import (
